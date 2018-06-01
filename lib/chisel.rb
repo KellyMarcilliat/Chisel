ad7883642fc5b398446450142f6b9ae411b16539
KellyMarcilliat <kelly.marcilliat@gmail.com>
2018-06-01 10:51:58 -0600

    WIP - changed read_lines and Write methods to require arguments; added translator class to handle substitutions of markdown tags with html tags

--- lib/chisel.rb
+++ lib/chisel.rb
@@ -2,37 +2,25 @@ require 'pry'
 
 
 class Chisel
-  attr_reader :input_file
+  attr_reader :input_file,
               :output_file
   def initialize(input_file, output_file)
     @input_file = input_file
     @output_file = output_file
   end
 
-  def read_lines
-    @lines_array = File.open(@input_file).readlines
+  def read_lines(read_file)
+    @lines_array = File.open(read_file).readlines
   end
 
-  def write
-    File.open(@output_file, 'w') {|text|
+  def write(write_file)
+    File.open(write_file, 'w') {|text|
       text.puts @lines_array
     }
+    return @lines_array
   end
-
-
 end
 
 chisel = Chisel.new('./lib/my_input.markdown', './lib/my_output.html')
-
-
-chisel.read_lines
-# p chisel.paragraph_tagging
-# p chisel.count_hashtags
-# p chisel.find_prepend_tag
-# p chisel.find_append_tag
-# p chisel.remove_hashtags
-# p chisel.remove_leading_space
-# p chisel.add_html_tags
-chisel.write
-
-# @hash_sub_opening["#{}"]
+  chisel.read_lines('./lib/my_input.markdown')
+  chisel.write('./lib/my_output.html')
--- test/chisel_test.rb
+++ test/chisel_test.rb
@@ -12,18 +12,17 @@ class ChiselTest < Minitest::Test
     assert_instance_of Chisel, chisel
   end
 
-  def test_read_generates_array_holding_strings_of_text_from_target_file
+  def test_it_creates_array_of_parsed_lines_from_input_file
     chisel = Chisel.new('./lib/input_for_test.markdown', './lib/output_for_test.html')
-
-    assert_equal ["totally interesting stuff\n",  "i want to see more!\n"],chisel.read_lines
+    assert_equal ["totally interesting stuff\n",  "i want to see more!\n", "\n", "1. sonoran dessert\n", "2. chihuahuan dessert\n"], chisel.read_lines('./lib/input_for_test.markdown')
   end
 
   def test_write_adds_text_to_file
     chisel = Chisel.new('./lib/input_for_test.markdown', './lib/output_for_test.html')
-      chisel.read_lines
-      chisel.write
+      chisel.read_lines('./lib/input_for_test.markdown')
+      chisel.write('./lib/output_for_test.html')
 
-    assert_equal ["totally interesting stuff\n",  "i want to see more!\n"], chisel.read_lines
+    assert_equal ["totally interesting stuff\n",  "i want to see more!\n", "\n", "1. sonoran dessert\n", "2. chihuahuan dessert\n"], chisel.read_lines('./lib/output_for_test.html')
   end
 
 
